385542986c019c096b674e0e87613a4c883814f1
bit <2656585116@qq.com>
2025-12-31 11:31:17 +0800

    finish 7.3

---
+++ ch007/接口库/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <a.h>
+
+int main()
+{
+    printf("%d\n", add(1, 2));
+    return 0;
+}
---
+++ ch007/目标文件库/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("hello CMake!\n");
+    printf("%d,%d\n", add(1, 2), sub(3,2));
+    return 0;
+}
